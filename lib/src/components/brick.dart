99c54b7c957526ce8760728a5a0df3c511964dc9
yyypluto <mfarrasashshiddiqi@gmail.com>
2024-09-09 10:05:49 +0700

    Creep

---
+++ lib/src/components/brick.dart
@@ -0,0 +1,44 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+import 'package:brick_breaker/audio/audio_controller.dart';
+
+import '../brick_breaker.dart';
+import '../config.dart';
+import 'ball.dart';
+import 'bat.dart';
+
+class Brick extends RectangleComponent
+    with CollisionCallbacks, HasGameReference<BrickBreaker> {
+  final AudioController audioController;
+
+  Brick({
+    required super.position,
+    required Color color,
+    required this.audioController,
+  }) : super(
+          size: Vector2(brickWidth, brickHeight),
+          anchor: Anchor.center,
+          paint: Paint()
+            ..color = color
+            ..style = PaintingStyle.fill,
+          children: [RectangleHitbox()],
+        );
+
+  @override
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollisionStart(intersectionPoints, other);
+    removeFromParent();
+    game.score.value++;
+
+    if (game.world.children.query<Brick>().length == 1) {
+      game.playState = PlayState.won;
+      game.world.removeAll(game.world.children.query<Ball>());
+      game.world.removeAll(game.world.children.query<Bat>());
+
+      // Play victory sound
+      audioController.startMusic();
+    }
+  }
+}
